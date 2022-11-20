9a57361aa3765b66fd620aaa04a632447f57d662
Arshad Karim <karim.arshad@gmail.com>
2022-11-19 20:52:36 -0500

    commit tree 31280ffe8e5354a5b53c6a121a1dbd6df579ff2d

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const name = "Joe";
-const height = 74;
+const name = "Ryan Trahan";
+const height = 5*12+6;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+console.log(message);
